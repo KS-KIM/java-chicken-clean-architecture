32ff0189854a00063b4b8b0e0c7a7d320a99fc31
K.S.KIM <nullable@kakao.com>
2021-12-24 22:13:15 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/ChickenPosApplication.java
@@ -0,0 +1,11 @@
+public class ChickenPosApplication {
+    public ChickenPosApplication() {
+    }
+
+    public static void main(String[] args) {
+        new ChickenPosApplication().run();
+    }
+
+    public void run() {
+    }
+}
